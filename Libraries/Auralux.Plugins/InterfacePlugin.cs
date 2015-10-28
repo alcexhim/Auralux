8888cd8e567d89d6eadb9e18ea2d4bfe1df1ecc3
alcexhim <alcexhim@gmail.com>
2015-10-27 23:06:21 -0400

    Start to implement Auralux plugin system

---
+++ Libraries/Auralux.Plugins/InterfacePlugin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auralux.Plugins
+{
+    public abstract class InterfacePlugin
+    {
+		public void Initialize()
+		{
+			InitializeInternal();
+		}
+		protected virtual void InitializeInternal()
+		{
+		}
+
+		public DevicePlugin[] GetDevices()
+		{
+			return GetDevicesInternal();
+		}
+		protected abstract DevicePlugin[] GetDevicesInternal();
+    }
+}
---
+++ Plugins/Interfaces/uDMX/Auralux.Plugins.Interfaces.uDMX/uDMXPlugin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Auralux.Plugins.Interfaces.uDMX
+{
+    public class uDMXPlugin : InterfacePlugin
+    {
+
+		protected override DevicePlugin[] GetDevicesInternal()
+		{
+			List<DevicePlugin> list = new List<DevicePlugin>();
+
+			return list.ToArray();
+		}
+	}
+}
